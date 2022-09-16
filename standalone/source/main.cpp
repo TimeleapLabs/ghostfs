fcd8fbda5e5e11e276f439d0ed480d65ebe71b19
Pouya <pouya.eghbali@yandex.com>
2022-09-17 00:00:14 +0200

    Add tls to server

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -33,6 +33,8 @@ auto main(int argc, char** argv) -> int {
     ("R,retries", "Authentication token retries", cxxopts::value<int64_t>()->default_value("-1"))
     ("w,write-back", "Write back cache size", cxxopts::value<uint8_t>()->default_value("8"))
     ("C,read-ahead", "Read ahead cache size", cxxopts::value<uint8_t>()->default_value("8"))
+    ("k,key", "TLS key", cxxopts::value<std::string>()->default_value(""))
+    ("T,cert", "TLS cert", cxxopts::value<std::string>()->default_value(""))
     ("A,authorize", "Run in authorizer mode")
     ("m,mount", "Soft mount a directory")
     ("U,unmount", "Soft unmount a directory")
@@ -78,11 +80,13 @@ auto main(int argc, char** argv) -> int {
     std::string root = result["root"].as<std::string>();
     std::string bind = result["bind"].as<std::string>();
     std::string suffix = result["suffix"].as<std::string>();
+    std::string key = result["key"].as<std::string>();
+    std::string cert = result["cert"].as<std::string>();
 
     uint16_t port = result["port"].as<uint16_t>();
     uint16_t auth_port = result["auth-port"].as<uint16_t>();
 
-    return start_rpc_server(bind, port, auth_port, root, suffix);
+    return start_rpc_server(bind, port, auth_port, root, suffix, key, cert);
 
   } else if (result["client"].as<bool>()) {
     std::string host = result["host"].as<std::string>();
