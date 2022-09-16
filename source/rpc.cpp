747a381a9c9bc351585305f86ee8663452ae6b1a
Pouya <pouya.eghbali@yandex.com>
2022-09-16 19:56:23 +0200

    Swap source <-> dest

--- source/rpc.cpp
+++ source/rpc.cpp
@@ -754,8 +754,8 @@ public:
 
     if (ino == 1) {
       for([[maybe_unused]] auto const& mount: *get_user_mounts(user)){
-        std::string source = mount.first;
-        std::string destination = mount.second;
+        std::string destination = mount.first;
+        std::string source = mount.second;
         std::string file_path = std::filesystem::path(root) / source;
 
         uint64_t file_ino;
